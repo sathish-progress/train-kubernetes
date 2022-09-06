3fb9fa9d5738885471eed20a93bcb651c28a0ab7
Sathish <sbabu@progress.com>
2022-09-06 17:57:23 +0530

    use shell out to run commands
    Signed-off-by: Sathish <sbabu@progress.com>

--- lib/train-kubernetes/kubectl_client.rb
+++ lib/train-kubernetes/kubectl_client.rb
@@ -1,3 +1,5 @@
+require 'mixlib/shellout'
+
 module TrainPlugins
   module TrainKubernetes
     class KubectlClient
@@ -8,13 +10,24 @@ module TrainPlugins
       end
 
       def execute(command, stdin: true, tty: true)
-        instruction = ["kubectl exec"].tap do |arr|
+        instruction = build_instruction(command, stdin, tty)
+        shell = Mixlib::ShellOut.new(instruction)
+        shell.run_command
+      end
+
+      private
+
+      def shell
+        @shell ||= Mixlib::ShellOut.new(instruction)
+      end
+
+      def build_instruction(command, stdin, tty)
+        ["kubectl exec"].tap do |arr|
           arr << '--stdin' if stdin
           arr << "--tty" if tty
           arr << pod if pod
           arr << command
         end.join("\s")
-        system(instruction)
       end
     end
   end
